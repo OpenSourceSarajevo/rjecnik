8b1aab378c077c71f8697dff6ae15af2546384f7
Enis Mulic <enis.s.mulic@gmail.com>
2024-05-04 23:30:01 +0200

    feat: setup supabase connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
+import { createClient } from "@/utils/supabase/server";
+
 import Card from "./components/card";
+
 import styles from "./page.module.css";
 
-const Home = () => {
-  const cards = [...Array(8)].map((e, i) => (
-    <Card
-      key={i}
-      title="Docs"
-      description="Find in-depth information about Next.js features and API."
-    />
+const Home = async () => {
+  const supabase = createClient();
+  const { data: words } = await supabase.from("words").select();
+
+  const cards = words?.map((e, i) => (
+    <Card key={i} title={e.word} description={e.meaning} />
   ));
 
   return (
-    <main className={styles.main}>
-      <div className={styles.grid}>{cards}</div>
-    </main>
+    <>
+      <main className={styles.main}>
+        <div className={styles.grid}>{cards}</div>
+      </main>
+    </>
   );
 };
 
