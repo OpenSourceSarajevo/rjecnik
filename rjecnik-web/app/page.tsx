c821ac65aace30cbaea544bc9ca730a8a381be65
Enis Mulic <enis.s.mulic@gmail.com>
2024-07-08 19:37:43 +0200

    chore: move dictionary to home page

--- rjecnik-web/app/page.tsx
+++ rjecnik-web/app/page.tsx
@@ -1,21 +1,23 @@
-import { createClient } from "@/utils/supabase/server";
+// import { createClient } from "@/utils/supabase/server";
 
-import Card from "./components/card";
+// import Card from "./components/card";
 
 import styles from "./page.module.css";
+import Dictionary from "./dictionary/page";
 
 const Home = async () => {
-  const supabase = createClient();
-  const { data: words } = await supabase.from("words").select();
+  // const supabase = createClient();
+  // const { data: words } = await supabase.from("words").select();
 
-  const cards = words?.map((e, i) => (
-    <Card key={e.id} id={e.id} title={e.word} description={e.meaning} />
-  ));
+  // const cards = words?.map((e, i) => (
+  //   <Card key={e.id} id={e.id} title={e.word} description={e.meaning} />
+  // ));
 
   return (
     <>
       <main className={styles.main}>
-        <div className={styles.grid}>{cards}</div>
+        {/* <div className={styles.grid}>{cards}</div> */}
+        <Dictionary />
       </main>
     </>
   );
