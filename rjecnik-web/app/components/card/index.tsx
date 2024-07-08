3db2207a1bb7629af35f8997daae8a6bdd331197
Enis Mulic <enis.s.mulic@gmail.com>
2024-07-08 18:35:54 +0200

    feat(routing): handle homonyms and slashes

--- rjecnik-web/app/components/card/index.tsx
+++ rjecnik-web/app/components/card/index.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 import styles from "./card.module.css";
 
 type CardProps = {
+  id: number;
   title: string;
   description: string;
 };
 
 const Card = (props: CardProps) => {
-  const { title, description } = props;
+  const { id, title, description } = props;
+
+  // In case the word contains a slash (/), e.g A/a
+  const word = title.split("/")[0];
 
   return (
-    <Link href={`/rjecnik/${title}`}>
+    <Link href={`/rjecnik/${word}/${id}`}>
       <div className={styles.card}>
         <h2>{title}</h2>
         <p>{description}</p>
--- rjecnik-web/app/dictionary/[word]/[id]/page.tsx
+++ rjecnik-web/app/dictionary/[word]/[id]/page.tsx
@@ -2,14 +2,13 @@ import { createClient } from "@/utils/supabase/server";
 
 import style from "./page.module.css";
 
-const Page = async ({ params }: { params: { word: string } }) => {
+const Page = async ({ params }: { params: { word: string; id: number } }) => {
   const supabase = createClient();
   const { data, error } = await supabase
     .from("words")
     .select()
-    .eq("word", params.word)
-    .limit(1)
-    .single();
+    .eq("id", params.id)
+    .maybeSingle();
 
   if (error) {
     console.log(error);
