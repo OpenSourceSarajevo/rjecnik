48a2a54e13e56d9e02c70b0c0869c9eecbbbad33
Enis Mulić <enis.s.mulic@gmail.com>
2024-10-23 19:04:05 +0200

    feat(analytics): setup vercel analytics

--- rjecnik-web/app/layout.tsx
+++ rjecnik-web/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 import Navbar from "./components/navbar";
@@ -21,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {/* <Navbar /> */}
         {children}
+        <Analytics />
       </body>
     </html>
   );
