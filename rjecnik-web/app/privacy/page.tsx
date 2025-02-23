c736b32c306fbfb698a792cf2f3a5da22f5323ec
Enis Mulic <enis.s.mulic@gmail.com>
2025-02-23 10:53:00 +0100

    refactor(web): move data into files

--- rjecnik-web/app/privacy/page.tsx
+++ rjecnik-web/app/privacy/page.tsx
@@ -6,6 +6,8 @@ import Footer from "@/app/components/Footer";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
 
+import { sections } from "./sections.data";
+
 const PrivacyPolicy = () => {
   const [isMobile, setIsMobile] = useState(false);
 
@@ -32,44 +34,6 @@ const PrivacyPolicy = () => {
       transition: { duration: 0.6 },
     },
   };
-
-  const sections = [
-    {
-      title: "Prikupljanje podataka",
-      content:
-        "Prikupljamo minimalne korisničke podatke radi poboljšanja funkcionalnosti web stranice, uključujući anonimne analitičke podatke putem Vercel Analytics za praćenje performansi.",
-    },
-    {
-      title: "Upotreba podataka",
-      content:
-        "Prikupljeni podaci nam pomažu da razumijemo kako korisnici koriste web stranicu, poboljšamo brzinu, upotrebljivost i sadržaj te održavamo sigurnost servisa.",
-    },
-    {
-      title: "Treće strane",
-      content:
-        "Koristimo Vercel Analytics za praćenje performansi, ali ovaj servis ne prikuplja lične podatke korisnika.",
-    },
-    {
-      title: "Kolačići",
-      content:
-        "Moguće je da koristimo kolačiće za analizu performansi web stranice. Kolačiće možete kontrolisati putem postavki vašeg pretraživača.",
-    },
-    {
-      title: "Sigurnost podataka",
-      content:
-        "Poduzimamo razumne mjere kako bismo zaštitili vaše podatke i spriječili neovlašteni pristup.",
-    },
-    {
-      title: "Vaša prava",
-      content:
-        "Pošto prikupljamo minimalne podatke, nemamo korisničke račune ili sistem praćenja korisnika. Ako imate pitanja, kontaktirajte nas za pojašnjenje.",
-    },
-    {
-      title: "Izmjene politike privatnosti",
-      content:
-        "Politika privatnosti može biti ažurirana. Sve izmjene će biti objavljene na ovoj stranici.",
-    },
-  ];
 
   return (
     <div className={styles.privacyContainer}>
