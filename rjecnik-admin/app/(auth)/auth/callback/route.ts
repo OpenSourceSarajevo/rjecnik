b88428b593b5aedce35b333227d4b38c1c99f13d
Enis Mulić <enis.s.mulic@gmail.com>
2025-06-13 20:32:13 +0200

    chore(admin): replace any type in auth route

--- rjecnik-admin/app/(auth)/auth/callback/route.ts
+++ rjecnik-admin/app/(auth)/auth/callback/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 
-export async function GET(request: { url: string | URL; headers: { get: (arg0: string) => any } }) {
+export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
