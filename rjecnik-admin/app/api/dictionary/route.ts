3c940a87033418417a35dd75d5c3c491b5e7a19c
Enis Mulić <enis.s.mulic@gmail.com>
2025-06-20 22:49:49 +0200

    feat(admin): add word editing page

---
+++ rjecnik-admin/app/api/dictionary/route.ts
@@ -0,0 +1,36 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export type Word = {
+  id: number;
+  headword: string;
+  definitions: Definition[];
+};
+
+type Definition = {
+  type: string | null;
+  gender: string | null;
+  examples: string[] | null;
+  definition: string;
+  part_of_speech: string | null;
+  pronunciation_ipa: string | null;
+  pronunciation_audio: string | null;
+};
+
+export async function GET(request: NextRequest) {
+  const pageNumber = +request.nextUrl.searchParams.get("pageNumber")!;
+  const pageSize = +request.nextUrl.searchParams.get("pageSize")!;
+  const word = request.nextUrl.searchParams.get("word") ?? "";
+
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.rpc("search_words_ranked", {
+    search_term: word,
+    limit_value: pageSize,
+    offset_value: pageNumber * pageSize,
+  });
+
+  if (error) throw error;
+
+  return NextResponse.json<Word[]>(data);
+}
