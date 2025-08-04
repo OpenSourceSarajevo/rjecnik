1c7514eafc468b925c15d4dc972398b751bc642a
Enis Mulić <enis.s.mulic@gmail.com>
2025-08-04 19:49:07 +0200

    feat(admin, processing): add frequency only processing strategy

---
+++ rjecnik-admin/app/api/words/new/[id]/strategy/[strategy]/strategies/strategyHandler.ts
@@ -0,0 +1,28 @@
+import { WordProcessingStrategy } from "@/app/api/words/contracts";
+import { applyFrequencyOnly } from "./frequencyOnly";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type HandlerFn = (id: number, body: any) => Promise<any>;
+
+const strategyMap: Record<WordProcessingStrategy, HandlerFn> = {
+  "New Definition": async () => ({}),
+  Ignore: async () => ({}),
+  "Frequency Only": applyFrequencyOnly,
+  "New Example": async () => ({}),
+  "New Form": async () => ({}),
+  "Existing Form": async () => ({}),
+  "New Headword": async () => ({}),
+  Remove: async () => ({}),
+};
+
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function handleStrategy(id: number, strategy: string, body: any) {
+  if (!(strategy in strategyMap)) {
+    throw new Error(`Nepoznata strategija: ${strategy}`);
+  }
+
+  const handler = strategyMap[strategy as WordProcessingStrategy];
+  return await handler(id, body);
+}
